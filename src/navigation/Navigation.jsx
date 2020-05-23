0c8f9ae282664b2c23cb9a73c083c8fe69efa3d6
unknown <gilyjmoon@gmail.com>
2020-05-23 16:12:10 +0300

    1

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,49 +1,45 @@
 import React, { Component } from "react";
-import {generateWeekRange} from '../functions'
+import { generateWeekRange } from "../functions";
 
 class Navigation extends Component {
-state = {
-  week: generateWeekRange(this.props.monday)
-}
 
-static getDerivedStateFromProps(props, state){
-  return state.week;
-}
 
+  
+  
 
   render() {
-    console.log(this.state.week);
-    
+    const week =  generateWeekRange(this.props.monday);
+
     return (
       <div className="week">
         <div className="day_date">
           <div className="day-header">Пн</div>
-    <div className="date">{this.state.week[0].getDate()}</div>
+          <div className="date">{week[0]}</div>
         </div>
-        <div className="day_date">
+        {/* <div className="day_date">
           <div className="day-header">Вт</div>
-          <div className="date">{this.state.week[1].getDate()}</div>
+          <div className="date">{aWeek[1]}</div>
         </div>
         <div className="day_date">
           <div className="day-header">Ср</div>
-          <div className="date">{this.state.week[2].getDate()}</div>
+          <div className="date">{aWeek[2]}</div>
         </div>
         <div className="day_date">
           <div className="day-header">Чт</div>
-          <div className="date">{this.state.week[3].getDate()}</div>
+          <div className="date">{aWeek[3]}</div>
         </div>
         <div className="day_date">
           <div className="day-header">Пт</div>
-          <div className="date">{this.state.week[4].getDate()}</div>
+          <div className="date">{aWeek[4]}</div>
         </div>
         <div className="day_date">
           <div className="day-header">Сб</div>
-          <div className="date">{this.state.week[5].getDate()}</div>
+          <div className="date">{aWeek[5]}</div>
         </div>
         <div className="day_date">
           <div className="day-header">Вс</div>
-          <div className="date">{this.state.week[6].getDate()}</div>
-        </div>
+          <div className="date">{aWeek[6]}</div>
+        </div> */}
       </div>
     );
   }
